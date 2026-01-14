e2d8cbd9635f04a1e7fdcaf9c7e34ddeb6f2f8d0
furkanerdogan <furkanerdd1@gmail.com>
2026-01-14 20:44:58 +0300

    chore: initialize Spring Boot project and Dockerized PostgreSQL

---
+++ src/main/java/com/coinguard/CoinguardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coinguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinguardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinguardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coinguard/CoinguardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinguardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

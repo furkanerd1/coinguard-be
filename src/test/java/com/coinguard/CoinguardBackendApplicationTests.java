d591b8f717d4d98229de749efc1ecaed994302a5
furkanerdogan <furkanerdd1@gmail.com>
2026-01-31 21:15:17 +0300

    feat(tests): enable Spring Data web support with DTO serialization for tests

--- src/test/java/com/coinguard/CoinguardBackendApplicationTests.java
+++ src/test/java/com/coinguard/CoinguardBackendApplicationTests.java
@@ -2,8 +2,12 @@ package com.coinguard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootTest
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class CoinguardBackendApplicationTests {
 
 	@Test
